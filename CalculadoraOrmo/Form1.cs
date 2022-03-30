b634c8aac2960361ae64ace99fe65345eb2c6af1
IsabelleOrmo <bellecrenonini@gmail.com>
2022-03-30 17:33:46 -0300

    um monte de alterações

--- CalculadoraOrmo/Form1.cs
+++ CalculadoraOrmo/Form1.cs
@@ -16,5 +16,84 @@ namespace CalculadoraOrmo
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Trim().Equals("0"))
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text = textBox1 + "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (!textBox1.Text.Trim().Equals("0"))
+            {
+                textBox1.Text += "0";
+            }
+            else
+            {
+                textBox1.Text = "0";
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
--- CalculadoraOrmo/Program.cs
+++ CalculadoraOrmo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//but we are two slow dancerssssssss last onde ao
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
